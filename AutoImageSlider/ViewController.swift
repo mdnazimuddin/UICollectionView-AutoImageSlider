79f69f755a3fb1b8f49c374f6e46c8a6dab7c5bf
Nazim Uddin <mdnazimuddincse@gmail.com>
2019-08-27 11:17:28 +0600

    Creat a New Project Auto Image Slider Using UICollectionViewCell

---
+++ AutoImageSlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoImageSlider
+//
+//  Created by Nazim Uddin on 27/8/19.
+//  Copyright © 2019 Nazim Uddin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
